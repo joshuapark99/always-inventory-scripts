8168351526dd76758aed20e136d9fc337ca1c5e0
Joshua Sungin Park <js4park@gmail.com>
2021-01-13 15:06:56 -0800

    Add files via upload

--- otherInfo.py
+++ otherInfo.py
@@ -19,7 +19,7 @@ lastUsedWBPath = None
 wb = None
 style_sheet = None
 
-def getInfo(sku,path,row,col):
+def getInfo(sku,path,row):
     global lastUsedWBPath
     global wb
     global style_sheet
@@ -45,29 +45,31 @@ def getInfo(sku,path,row,col):
 
 
 
-    for col2 in range(col,style_sheet.max_column+1):
+    for col2 in range(1,style_sheet.max_column+1):
         if style_sheet.cell(3,col2).value in other_info:
             object[style_sheet.cell(3,col2).value] = style_sheet.cell(row,col2).value
     #wb.close()
     return object
 
 if __name__ == "__main__":
+    #global lastUsedWBPath
     m_wb = openpyxl.load_workbook(masterSheet,read_only=True)
     new_wb = openpyxl.Workbook()
-
+    path = ''
     read_sheet = m_wb['Sheet']
 
     write_sheet = new_wb['Sheet']
 
     for counter in range(2,read_sheet.max_row+1):
         sku = read_sheet.cell(counter,1).value
-        path = read_sheet.cell(counter,3).value
+        if(path != lastUsedWBPath):
+            path = read_sheet.cell(counter,3).value
         row = read_sheet.cell(counter,4).value
-        col = read_sheet.cell(counter,5).value
-        object = getInfo(sku, path, row, col)
-        write_sheet.cell(counter,1).value = read_sheet.cell(counter,1).value
+        #col = read_sheet.cell(counter,5).value
+        object = getInfo(sku, path, row)
+        write_sheet.cell(counter,1).value = sku
         write_sheet.cell(counter,2).value = read_sheet.cell(counter,2).value
-        write_sheet.cell(counter,3).value = read_sheet.cell(counter,3).value
+        write_sheet.cell(counter,3).value = path
         for x,y in enumerate(other_info,start=6):
             if object[y] != "not here":
                 write_sheet.cell(counter,x).value = object[y]
